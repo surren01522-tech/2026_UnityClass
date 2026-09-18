535940d7df2677960728b863de767451897d91b6
surren01522-tech <surren01522@gmail.com>
2026-09-18 17:08:32 +0900

    [feat] 스크립트 생성

---
+++ UnityClass/Assets/Scripts/Class_01.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Class_01 : MonoBehaviour
+{
+    int level = 1;
+    float damage = 1.5f;
+    string palyer = "플레이어";
+    bool isDead = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
